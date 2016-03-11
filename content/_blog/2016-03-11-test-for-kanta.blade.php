c3f02ba2d25852eda5b5924b47a50fbcc2b4ef5c
Specs <464089419@qq.com>
2016-03-11 14:08:09 +0800

    update

---
+++ content/_blog/2016-03-11-test-for-kanta.blade.php
@@ -0,0 +1,14 @@
+@extends('_includes.blog_post_base')
+
+@section('post::title', 'Just a test for Katana')
+@section('post::date', 'March 11, 2016')
+@section('post::brief', 'Just a test for Katana')
+@section('pageTitle')- @yield('post::title')@stop
+
+@section('post_body')
+
+    @markdown
+    #Just a test for Katana
+    @endmarkdown
+
+@stop
